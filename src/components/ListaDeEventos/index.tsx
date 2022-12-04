d87a3b4864c62c2a8ae1f2c373beced3f43fd592
leonardofigueiro <leonardo.figueiro@yahoo.com.br>
2022-12-04 11:04:57 -0400

    first commit

---
+++ src/components/ListaDeEventos/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { IFiltroDeEventos } from '../../interfaces/IFiltroDeEventos';
+import { filtroDeEventos } from '../../state/atom';
+import useListaDeEventos from '../../state/hooks/useListaDeEventos';
+import Evento from '../Evento';
+import Filtro from '../Filtro';
+import style from './ListaDeEventos.module.scss';
+
+const ListaDeEventos: React.FC = () => {
+
+  const eventos = useListaDeEventos();
+
+  return (<section>
+    <Filtro />
+    <div className={style.Scroll}>
+      {eventos.map(evento => (
+        <Evento evento={evento} key={evento.id} />
+      ))}
+    </div>
+  </section>)
+}
+
+export default ListaDeEventos
